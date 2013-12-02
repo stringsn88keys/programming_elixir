b5074243f0b54274f2e63099e0d497293a6be2b9
Thomas Powell <tpowell@appriss.com>
2013-12-01 22:47:52 -0500

    Added ranges to solution.

--- exercises/ListsAndRecursion-3.exs
+++ exercises/ListsAndRecursion-3.exs
@@ -1,7 +1,12 @@
 defmodule MyList do
+
   def caesar(list, n), do: _caesar(list, n)
   defp _caesar([head|[]], n), do: [_rotn(head, n)]
   defp _caesar([head|tail], n), do: [_rotn(head, n) | _caesar(tail, n)]
-  defp _rotn(char, n) when char + n <= ?z, do: char + n
-  defp _rotn(char, n) when char + n > ?z, do: char + n - 26
+  defp _rotn(char, n) when char in ?A..?Z do
+    rem(char - ?A + n, 26) + ?A
+  end
+  defp _rotn(char, n) when char in ?a..?z do
+    rem(char - ?a + n, 26) + ?a
+  end
 end
